650a4a9c1cdd718f326d9b095e172ed5a42269c6
Ephrata <ephrata.ak@gmail.com>
2022-08-28 17:38:09 +0300

    route added

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 const pool = require("./Server/configure/database")
-// const userRouter = require("./server/api/users/user.router");
+const userRouter = require("./Server/api/users/user.router")
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));//data from front end
 app.use(express.json());
-// app.use("/api/users", userRouter);
+app.use("/api/users", userRouter);
+
 
 app.listen(port, () => console.log(`Listening http://localhost:${port}`));
